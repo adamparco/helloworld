10fa3e60ae25fbc805aade844c5ef934c6a659db
Adam Parco <adamparco@Adockam.tsid.io>
2019-04-24 10:58:04 -0400

    adding uname

--- src/hello.py
+++ src/hello.py
@@ -1,9 +1,10 @@
+import platform
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello from %s" % platform.uname()[4]
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
